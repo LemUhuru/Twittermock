21a943642510ff001ef773c36eb5ec22a8397c08
Lemuel <lemueluhuru@gmail.com>
2014-09-01 00:35:49 -0400

    complete ch 9 exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy]
+  before_action :not_signed_in_user, only: [:new, :create]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -39,9 +40,12 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
-    redirect_to users_url 
+    user = User.find(params[:id])
+    unless current_user?(user)
+      user.destroy
+      flash[:success] = "User deleted."
+    end
+    redirect_to users_url
   end
 
 private
@@ -57,6 +61,11 @@ def signed_in_user
     store_location
     redirect_to signin_url, notice: "Please sign in."
   end
+end
+
+
+def not_signed_in_user
+  redirect_to root_url if signed_in?
 end
 
 def correct_user 
