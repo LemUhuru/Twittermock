e2dc65b418cb1104c554d69be131394893f06733
Lemuel <lemueluhuru@gmail.com>
2014-09-11 21:14:08 -0400

    correct user model, specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -185,15 +185,9 @@ describe User do
     its(:followed_users) { should include(other_user) }
 
     describe "followed user" do 
-<<<<<<< HEAD
-        subject { other_user }
-        its(:followers) { should include(@user) }
-      end
-=======
       subject { other_user }
       its(:followers) { should include(@user) }
     end
->>>>>>> following-users
 
     describe "and unfollowing" do 
       before { @user.unfollow!(other_user) }
